3e9b34559b6c43dd3b5b060345f1fa24021f4ae9
ajhxk <ajhxk@outlook.com>
2020-05-11 10:42:37 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import avatar from './avatar.jpg';
-import style from './index.less';
-import createAvatar from './createAvatar'
-
-console.log('style:  ', JSON.stringify(style))
-createAvatar();
-
-var img = new Image();
-img.src = avatar;
-img.classList.add(style.avatar);
-
 var root = document.getElementById('root');
-root.append(img);
+import './index.less';
+
+root.innerHTML = '<div class="iconfont icon-changjingguanli"></div>';
